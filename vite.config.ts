a164362e21b6750ef5415955c79284ef9d3ea660
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2025-02-07 11:38:09 +0100

    completion of landing page

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  base: "/",
   build: {
     ssr: false, // Ensure it's a static site
     outDir: "dist",

bffe9ed3cf028bee46c815fa907c5b11163a2196
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2025-02-07 11:16:43 +0100

    completion of landing page

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,3 @@
-// import { reactRouter } from "@react-router/dev/vite";
-// import tailwindcss from "@tailwindcss/vite";
-// import { defineConfig } from "vite";
-// import tsconfigPaths from "vite-tsconfig-paths";
-
-// export default defineConfig({
-//   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-// });
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
@@ -13,8 +5,4 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  server: {
-    host: "0.0.0.0", // Ensure it binds to all network interfaces
-    port: 3000, // Set the port
-  },
 });
